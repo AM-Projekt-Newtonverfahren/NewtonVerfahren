d84be6e261f805f66566fa9c07cc5a4c5d6c5ca4
Leandro200409 <78601676+Leandro200409@users.noreply.github.com>
2022-12-09 08:03:50 +0100

    First commit of the application

---
+++ newtonverfahren_project/main.py
@@ -0,0 +1,15 @@
+import sys
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy
+from PyQt5.QtWidgets import QApplication, QLabel
+
+def app_test():
+    app = QApplication([])
+    label = QLabel("Hello world")
+    label.show()
+    app.exec_()
+
+def ableitenPolynom(funktion):
+
+
