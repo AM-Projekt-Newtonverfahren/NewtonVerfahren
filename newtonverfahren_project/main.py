6be7748c358fe2dfbc07dc3a0d38903e906fba4e
Leandro200409 <78601676+Leandro200409@users.noreply.github.com>
2022-12-16 09:14:07 +0100

    Ableitungsmethode für Polynomfunktionen implementiert.

--- newtonverfahren_project/main.py
+++ newtonverfahren_project/main.py
@@ -4,12 +4,22 @@ import matplotlib.pyplot as plt
 import numpy
 from PyQt5.QtWidgets import QApplication, QLabel
 
+
 def app_test():
     app = QApplication([])
     label = QLabel("Hello world")
     label.show()
     app.exec_()
 
-def ableitenPolynom(funktion):
-    pass
 
+def ableitenPolynom(basis, exponent):
+    newBasis = basis*exponent;
+    newExponent = exponent-1;
+
+    if(exponent == 0):
+        print(basis)
+    else:
+        print(str(newBasis)+"x^"+str(newExponent))
+
+print(numpy.sin(numpy.deg2rad(2)))
+ableitenPolynom(1, numpy.sin(numpy.deg2rad(2)))
