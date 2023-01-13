12e75694890540779a2931177503f89978f97cec
V3rlassen <78602042+V3rlassen@users.noreply.github.com>
2023-01-13 07:58:07 +0100

    funktion

--- newtonverfahren_project/main.py
+++ newtonverfahren_project/main.py
@@ -4,12 +4,33 @@ import matplotlib.pyplot as plt
 import numpy
 from PyQt5.QtWidgets import QApplication, QLabel
 
+
 def app_test():
     app = QApplication([])
     label = QLabel("Hello world")
     label.show()
     app.exec_()
 
-def ableitenPolynom(funktion):
+
+def ableitenPolynom(exponent):
+    newBasis = exponent;
+    newExponent = exponent-1;
+
+    if(exponent == 0):
+        print(0)
+    else:
+        print(str(newBasis)+"x^"+str(newExponent))
+
+def ableitenExponent(basis):
+    print(str(basis)+"^x*LOGe("+str(basis)+")")
+
+#interpreter
+def SteigungBerechnen(startwert, funktion):
+    if(funktion.__contains__("x^")):
+        pass
+
+def yAchsenabschnittBerechnen(startwert, funktion):
     pass
 
+ableitenPolynom(4)
+ableitenExponent(3)
