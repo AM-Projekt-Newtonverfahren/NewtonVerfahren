d0de9b2faf55f713c8c7983e329da853846c2dca
Leandro200409 <78601676+Leandro200409@users.noreply.github.com>
2023-01-13 09:12:07 +0100

    functions added

--- newtonverfahren_project/main.py
+++ newtonverfahren_project/main.py
@@ -2,11 +2,11 @@ import sys
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy
-from PyQt5.QtWidgets import QApplication, QLabel, QWidget, QHBoxLayout, QScrollArea
+#from PyQt5.QtWidgets import QApplication, QLabel, QWidget, QHBoxLayout, QScrollArea
 
 
 
-def app_test():
+"""def app_test():
     app = QApplication([])
     window = QWidget()
     layoutPrinciple = QHBoxLayout();
@@ -15,7 +15,7 @@ def app_test():
     layoutPrinciple.addWidget(scrollArea)
     window.setLayout(layoutPrinciple)
     window.show()
-    app.exec_()
+    app.exec_()"""
 
 def ableitenPolynom(basis, exponent):
     newBasis = basis*exponent;
@@ -25,10 +25,21 @@ def ableitenPolynom(basis, exponent):
         print(0)
     else:
         print(str(newBasis)+"x^"+str(newExponent))
+        return str(newBasis)+"x^"+str(newExponent)
 
-def yAchsenabschnittBerechnen(startwert, basis, exponent):
+def yAchsenabschnittBerechnenStartwert(startwert, basis, exponent):
     achse = (basis*startwert)**exponent
     print(achse)
+    return achse
+
+def yAchesnabschnittBerechnenTangente(yAchse, startwert, ableitung, basis, exponent):
+    if(ableitung.__contains__("x^")):
+        parts = ableitung.split("x^")
+        function = (basis*startwert)**exponent
+        ableitungMitStartwert = (int(parts[0])*startwert)**int(parts[1])
+        steigung = function-ableitungMitStartwert
+        print(steigung)
+        return steigung
     
 def ableitenExponent(basis):
     print(str(basis)+"^x*LOGe("+str(basis)+")")
@@ -36,9 +47,20 @@ def ableitenExponent(basis):
 #interpreter
 def SteigungBerechnen(startwert, funktion):
     if(funktion.__contains__("x^")):
-        pass
+        parts = funktion.split("x^")
+        return int(parts[0])*(startwert**int(parts[1]))
+
+def getNewStartwert(achse, steigung):
+    newAchse = achse*(-1)
+    wert = newAchse/steigung
+    print(wert)
 
 
-app_test()
-ableitenPolynom(2, 1)
-yAchsenabschnittBerechnen(1, 2, 3)
+#app_test()
+ableitung = ableitenPolynom(2, 3)
+achse = yAchsenabschnittBerechnenStartwert(1, 2, 3)
+achseTangente = yAchesnabschnittBerechnenTangente(achse, 1, ableitung, 2, 3)
+steigung = SteigungBerechnen(1, ableitung)
+getNewStartwert(achseTangente, steigung)
+
+
